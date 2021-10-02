47d236b4d72a5874104ececbd602f8cb35d008df
Johann Schopplich <mail@johannschopplich.com>
2021-10-02 18:34:39 +0200

    Update deps with Composer 2

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInitd592f846d44b411bfe2ccc611e92a733
         }
 
         spl_autoload_register(array('ComposerAutoloaderInitd592f846d44b411bfe2ccc611e92a733', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitd592f846d44b411bfe2ccc611e92a733', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,33 +1,32 @@
-<?php return array (
-  'root' => 
-  array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
-    'aliases' => 
-    array (
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'kirby-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9e74e9c90edcc7a042bea4e3216c73c6a2b08513',
+        'name' => 'lukaskleinschmidt/kirby-terminal',
+        'dev' => true,
     ),
-    'reference' => 'b4fbb99c14fdfda7c64a347852a60500a566d05d',
-    'name' => 'lukaskleinschmidt/kirby-terminal',
-  ),
-  'versions' => 
-  array (
-    'getkirby/composer-installer' => 
-    array (
-      'pretty_version' => '1.2.0',
-      'version' => '1.2.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '240a8b2c275d61b66601feb58222b7d34bc6cf1e',
+    'versions' => array(
+        'getkirby/composer-installer' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../getkirby/composer-installer',
+            'aliases' => array(),
+            'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
+            'dev_requirement' => false,
+        ),
+        'lukaskleinschmidt/kirby-terminal' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'kirby-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9e74e9c90edcc7a042bea4e3216c73c6a2b08513',
+            'dev_requirement' => false,
+        ),
     ),
-    'lukaskleinschmidt/kirby-terminal' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-      ),
-      'reference' => 'b4fbb99c14fdfda7c64a347852a60500a566d05d',
-    ),
-  ),
 );
